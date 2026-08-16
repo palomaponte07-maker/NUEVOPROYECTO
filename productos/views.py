177b0134069897c5ef53ed4c6c2734cb4681e61f
Melani <melaniponte04@gmail.com>
2026-08-16 01:08:59 -0300

    Conectar inicio con productos de la BD

--- productos/views.py
+++ productos/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Producto
 
 def inicio(request):
-    return render(request, "cliente/inicio.html")
+    productos_destacados = Producto.objects.filter(porcentajeDescuento__gt=0)
+
+    return render(
+        request,
+        "cliente/inicio.html",
+        {"productos_destacados": productos_destacados}
+    )
 
 def productos(request):
     return render(request, "cliente/productos.html")
-

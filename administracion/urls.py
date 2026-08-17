48f06e8235e8924b7cb86c4abf1e5ed3dfa8f8d9
Paloma <palomaponte07@gmail.com>
2026-08-16 22:38:53 -0300

    Actualizo configuración

--- administracion/urls.py
+++ administracion/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path 
 from .views import dashboard 
+from .views import dashboard, agregar_producto
 
 urlpatterns = [ 
     path('', dashboard, name='dashboard')
+]
+urlpatterns = [
+    path('', dashboard, name='dashboard'),
+    path('productos/agregar/', agregar_producto, name='agregar_producto'),
 ]
--- administracion/views.py
+++ administracion/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, 'administracion/dashboard.html')
+
+def agregar_producto(request):
+    return render(request, 'administracion/productos/agregar.html')

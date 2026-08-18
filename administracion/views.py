917a78f4939e06931d8468a215352cf3657c1d98
Paloma <palomaponte07@gmail.com>
2026-08-18 14:45:53 -0300

    Ajuste de productos

--- administracion/views.py
+++ administracion/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, redirect
 from .forms import ProductoForm
+from productos.models import Producto, Imagen
 
 def dashboard(request):
-    return render(request, 'administracion/dashboard.html')
+    productos = Producto.objects.all().order_by('-idProducto')[:10]
+    return render(
+        request, 
+        'administracion/dashboard.html',
+        {'productos': productos} )
 
 def agregar_producto(request):
     if request.method == 'POST':
         form = ProductoForm(request.POST)
 
         if form.is_valid():
-            form.save()
+            producto =form.save(commit=False)
+            producto.precioDescuento = producto.precioOriginal
+            producto.porcentajeDescuento = 0
+            producto.save()
             return redirect('dashboard')
 
     else:
